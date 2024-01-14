28980f76fb1dbfae01fd28d27858ea6d60edd24f
jossyahia <jossyahia@gmail.com>
2024-01-14 20:51:19 +0100

    sw

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,70 +1,69 @@
-import { useState } from "react";
+import { categories } from "@data";
 import { IoIosImages } from "react-icons/io";
 import { BiTrash } from "react-icons/bi";
 
-import categories from "./data/categories";
-import "./styles/Form.scss";
+import "@styles/Form.scss";
 
-const Form = ({ type, initialWork, onSubmit }) => {
-  const [work, setWork] = useState(initialWork);
-
+const Form = ({ type, work, setWork, handleSubmit }) => {
   const handleUploadPhotos = (e) => {
     const newPhotos = e.target.files;
-
-    setWork((prevWork) => ({
-      ...prevWork,
-      photos: [...prevWork.photos, ...newPhotos],
-    }));
+    setWork((prevWork) => {
+      return {
+        ...prevWork,
+        photos: [...prevWork.photos, ...newPhotos],
+      };
+    });
   };
 
-  const handleRemovePhoto = (index) => {
-    setWork((prevWork) => ({
-      ...prevWork,
-      photos: prevWork.photos.filter((_, i) => i !== index),
-    }));
+  const handleRemovePhoto = (indexToRemove) => {
+    setWork((prevWork) => {
+      return {
+        ...prevWork,
+        photos: prevWork.photos.filter((_, index) => index !== indexToRemove),
+      };
+    });
   };
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-
-    setWork((prevWork) => ({
-      ...prevWork,
-      [name]: value,
-    }));
+    setWork((prevWork) => {
+      return {
+        ...prevWork,
+        [name]: value,
+      };
+    });
   };
 
   return (
     <div className="form">
       <h1>{type} Your Work</h1>
-
-      <form onSubmit={onSubmit}>
-        <h3>Which category describes your work?</h3>
-
+      <form onSubmit={handleSubmit}>
+        <h3>Which of these categories best describes your work?</h3>
         <div className="category-list">
-          {categories.map((item, index) => (
+          {categories?.map((item, index) => (
             <p
               key={index}
-              className={work.category === item ? "selected" : ""}
-              onClick={() => setWork({ ...work, category: item })}
+              className={`${work.category === item ? "selected" : ""}`}
+              onClick={() => {
+                setWork({ ...work, category: item });
+              }}
             >
               {item}
             </p>
           ))}
         </div>
 
-        <h3>Add photos of your work</h3>
-
+        <h3>Add some photos of your work</h3>
         {work.photos.length < 1 && (
           <div className="photos">
             <input
               id="image"
               type="file"
+              style={{ display: "none" }}
               accept="image/*"
               onChange={handleUploadPhotos}
               multiple
-              style={{ display: "none" }}
             />
-
             <label htmlFor="image" className="alone">
               <div className="icon">
                 <IoIosImages />
@@ -76,29 +75,26 @@ const Form = ({ type, initialWork, onSubmit }) => {
 
         {work.photos.length > 0 && (
           <div className="photos">
-            {work.photos.map((photo, index) => (
+            {work?.photos?.map((photo, index) => (
               <div key={index} className="photo">
                 {photo instanceof Object ? (
                   <img src={URL.createObjectURL(photo)} alt="work" />
                 ) : (
                   <img src={photo} alt="work" />
                 )}
-
                 <button type="button" onClick={() => handleRemovePhoto(index)}>
                   <BiTrash />
                 </button>
               </div>
             ))}
-
             <input
               id="image"
               type="file"
+              style={{ display: "none" }}
               accept="image/*"
               onChange={handleUploadPhotos}
               multiple
-              style={{ display: "none" }}
             />
-
             <label htmlFor="image" className="together">
               <div className="icon">
                 <IoIosImages />
@@ -108,43 +104,40 @@ const Form = ({ type, initialWork, onSubmit }) => {
           </div>
         )}
 
-        <h3>What makes your work attractive?</h3>
-
+        <h3>What make your Work attractive?</h3>
         <div className="description">
           <p>Title</p>
           <input
             type="text"
             placeholder="Title"
+            onChange={handleChange}
             name="title"
             value={work.title}
-            onChange={handleChange}
             required
           />
-
           <p>Description</p>
           <textarea
+            type="text"
             placeholder="Description"
+            onChange={handleChange}
             name="description"
             value={work.description}
-            onChange={handleChange}
             required
           />
-
-          <p>Set your price</p>
-          <span>#</span>
+          <p>Now, set your PRICE</p>
+          <span>$</span>
           <input
             type="number"
             placeholder="Price"
+            onChange={handleChange}
             name="price"
             value={work.price}
-            onChange={handleChange}
-            className="price"
             required
+            className="price"
           />
         </div>
-
-        <button type="submit" className="submit_btn">
-          Publish Your Work
+        <button className="submit_btn" type="submit">
+          PUBLISH YOUR WORK
         </button>
       </form>
     </div>
