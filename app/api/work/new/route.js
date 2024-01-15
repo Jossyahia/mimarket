73731e01760fc8651f7dfe8dca526b772f9774e2
jossyahia <jossyahia@gmail.com>
2024-01-15 02:34:07 +0100

    uplod_but_no

--- app/api/work/new/route.js
+++ app/api/work/new/route.js
@@ -1,56 +1,86 @@
 import { connectToDB } from "@mongodb/database";
-import { writeFile } from "fs/promises"
+import { writeFile } from "fs/promises";
 import Work from "@models/Work";
+import { createClient } from "@supabase/supabase-js";
+import uniqid from "uniqid";
 
-export async function POST (req) {
+export async function POST(req) {
   try {
     /* Connect to MongoDB */
-    await connectToDB()
+    await connectToDB();
 
-    const data = await req.formData()
+    const data = await req.formData();
 
+    // Create Supabase client
+    const supabase = createClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+    );
     /* Extract info from the data */
-    const creator = data.get("creator")
-    const category = data.get("category")
-    const title = data.get("title")
-    const description = data.get("description")
-    const price = data.get("price")
+    const creator = data.get("creator");
+    const category = data.get("category");
+    const title = data.get("title");
+    const description = data.get("description");
+    const price = data.get("price");
 
     /* Get an array of uploaded photos */
-    const photos = data.getAll("workPhotoPaths")
-
-    const workPhotoPaths = []
+    const photos = data.getAll("workPhotoPaths");
+    const bucket = "fastfastfood";
+      const workPhotoPaths = [];
 
     /* Process and store each photo  */
     for (const photo of photos) {
-      // Read the photo as an ArrayBuffer
-      const bytes = await photo.arrayBuffer()
+      console.log(photo);
 
+      const ext = photo.name.split(".").pop();
+      const newphotosName = uniqid() + "." + ext;
       // Convert it to a Buffer
-      const buffer = Buffer.from(bytes)
+      // Read the photo as an ArrayBuffer
+      const bytes = await photo.arrayBuffer();
+
+      const buffer = Buffer.from(bytes);
 
       // Define the destination path for the uploaded file
-      const workImagePath = `C:/Users/HP/Desktop/mimarket/public/uploads/${photo.name}`
+      // Upload file
+      const uploadResult = await supabase.storage
+        .from(bucket)
+        .upload(newphotosName, workPhotoPaths);
+      const { error } = uploadResult;
+
+      /// const workImagePath = `C:/Users/HP/Desktop/mimarket/public/uploads/${photo.name}`;
 
       // Write the buffer to the filessystem
-      await writeFile(workImagePath, buffer)
+      //await writeFile(workImagePath, buffer);
 
       // Store the file path in an array
-      workPhotoPaths.push(`/uploads/${photo.name}`)
+      // workPhotoPaths.push(`/uploads/${photo.name}`);
+      if (error) {
+        return Response.json({ error });
+      }
+      const link =
+        "https://frsmcjzgunhzcsxffmyj.supabase.co/storage/v1/object/public/" +
+        bucket +
+        "/" +
+        newphotosName;
+      console.log(link);
+      workPhotoPaths.push(link);
     }
 
     /* Create a new Work */
     const newWork = new Work({
-      creator, category, title, description, price, workPhotoPaths
-    })
+      creator,
+      category,
+      title,
+      description,
+      price,
+      workPhotoPaths,
+    });
 
-    await newWork.save()
+    await newWork.save();
 
-    return new Response(JSON.stringify(newWork), { status: 200 })
-  }
-  catch (err) {
-    console.log(err)
-    return new Response("Failed to create a new Work", { status: 500 })
+    return new Response(JSON.stringify(newWork), { status: 200 });
+  } catch (err) {
+    console.log(err);
+    return new Response("Failed to create a new Work", { status: 500 });
   }
 }
-
