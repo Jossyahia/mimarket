ba5d951e08b267a2e7d88d6e8f5374cec5414b95
jossyahia <jossyahia@gmail.com>
2024-02-05 02:24:43 +0100

    use client

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e083758968eae41e"; }
+/******/ 		__webpack_require__.h = function() { return "b736b6869cb3b299"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- app/wishlist/page.jsx
+++ app/wishlist/page.jsx
@@ -1,3 +1,4 @@
+"use client";
 import Navbar from "@components/Navbar";
 import Loader from "@components/Loader";
 import WorkList from "@components/WorkList";
